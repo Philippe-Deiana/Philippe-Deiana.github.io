f7d293d62920a42632f6dca8f57388a242f5e8e5
Philippe-Deiana <philippe.deiana@etu.umontpellier.fr>
2020-04-01 20:58:22 +0200

    maj

--- my_web_site/flutter_service_worker.js
+++ my_web_site/flutter_service_worker.js
@@ -1,17 +1,17 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "/assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"/assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
-"/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"/assets/LICENSE": "964211db6a8b173b1744e68da77ce459",
-"/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"/favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"/index.html": "8abc26589506106799db4682ca6fe168",
-"/main.dart.js": "2f0a7b6f1508169e628746c850acb2de",
-"/manifest.json": "07f61288bf15274d535584be5507a086"
+  "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+"assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"assets/LICENSE": "6818eb29f5caca5efc332f0816004c23",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"index.html": "8abc26589506106799db4682ca6fe168",
+"main.dart.js": "bb887ba6c2ef982742afa49004cb91c8",
+"manifest.json": "07f61288bf15274d535584be5507a086"
 };
 
 self.addEventListener('activate', function (event) {
